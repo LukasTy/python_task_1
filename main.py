168bfbc2a426157d0bcbef74ddae885c22215f02
LukasTy <llukas.tyla@gmail.com>
2014-04-08 02:42:55 +0300

    Add outputTotalResults function and and the needed variables for it to work.

--- main.py
+++ main.py
@@ -8,8 +8,10 @@
 # Imports
 import os, sys, string, re
 from collections import Counter
-#Global variable
+# Global variables
 counter = 0
+allWords = None
+allCharacters = None
 # The main program logic. Asks for a path and calls the function to handle main logic
 def mainLogic():
 	path = input("Specify the directory path (current directory is '.'): ")
@@ -24,6 +26,8 @@ def mainLogic():
 		print "You listed a wrong directory path"
 # Read file contents and call outputResults function to push the results to file
 def parseFile(fileName, path):
+	global allWords
+	global allCharacters
 	try:
 		if os.path.isfile(os.path.join(path, fileName)):
 			fileObject = open(os.path.join(path, fileName), 'r')
@@ -31,6 +35,10 @@ def parseFile(fileName, path):
 			# Find words
 			words = re.findall(r"[\w]{2,}", fileText)
 			wordCollection = Counter(words);
+			if allWords is not None:
+				allWords.update(wordCollection)
+			else:
+				allWords = wordCollection
 			outputContent = "===========Word details of {} file===========\n".format(fileName)
 			for word, repetitions in sorted(wordCollection.items(), key=lambda x:x[1]):
 				outputContent += word + " mentioned: " + str(repetitions) + "\n"
@@ -38,6 +46,10 @@ def parseFile(fileName, path):
 			# Find printable characters
 			text = filter(lambda x: x in string.printable, fileText)
 			characterCollection = Counter(text)
+			if allCharacters is not None:
+				allCharacters.update(characterCollection)
+			else:
+				allCharacters = characterCollection
 			outputContent = "===========Character details of {} file===========\n".format(fileName)
 			for character, repetitions in sorted(characterCollection.items(), key=lambda x:x[1]):
 				outputContent += character + " mentioned: " + str(repetitions) + "\n"
@@ -65,5 +77,29 @@ def outputResults(content):
 	finally:
 		outputFile.close()
 		counter += 1
+def outputTotalResults():
+	global allWords
+	global allCharacters
+	try:
+		outputFile = open("results.txt", "a")
+		outputWordsContent = "\n==========TOTAL word details for all files==========\n\n"
+		for word, repetitions in sorted(allWords.items(), key=lambda x:x[1]):
+			outputWordsContent += word + " mentioned: " + str(repetitions) + "\n"
+		outputFile.write(outputWordsContent + "\n")
+		outputCharactersContent = "\n==========TOTAL character details for all files==========\n\n"
+		for character, repetitions in sorted(allCharacters.items(), key=lambda x:x[1]):
+			outputCharactersContent += character + " mentioned: " + str(repetitions) + "\n"
+		outputFile.write(outputCharactersContent)
+	except IOError as e:
+		print "I/O error({0}): {1}".format(e.errno, e.strerror)
+	except:
+		print "Unexpected error:", sys.exc_info()[0]
+		raise
+	finally:
+		outputFile.close()
+def main():
+	mainLogic()
+	outputTotalResults()
 
-mainLogic()
+if __name__ == "__main__":
+	main()
